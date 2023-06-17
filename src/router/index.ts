678d0d05879a439f1ba34f976c2b75528ffd503f
trasta <trasta298@gmail.com>
2023-06-17 09:57:35 +0900

    ⚡️ add router

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const Home = () => import('../pages/HelloWorld.vue')
+const Login = () => import('../pages/Login.vue')
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    component: Login,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
