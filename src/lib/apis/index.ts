fb376903f4c183e1b6a973e597f3a834a8450b82
trasta <trasta298@gmail.com>
2023-06-18 17:22:27 +0900

    ⚡️ chg url

--- src/lib/apis/index.ts
+++ src/lib/apis/index.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-restricted-imports
 import { Apis, Configuration } from './generated'
 
-const apis = new Apis(new Configuration({ basePath: 'https://api.shiika.trasta.dev' }))
+const apis = new Apis(new Configuration({ basePath: 'https://shiikaback.trap.show' }))
 
 export default apis
 // eslint-disable-next-line no-restricted-imports
